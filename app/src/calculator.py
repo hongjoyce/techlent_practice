83b45548fa037ed9f79f8b8cb625926c96943c1f
hongjoyce <hongjoycejiang@gmail.com>
2022-08-14 18:11:28 -0500

    add html and flask

--- app/src/calculator.py
+++ app/src/calculator.py
@@ -1,7 +1,7 @@
 class Calculator():
     def add(self, num1, num2):
         return num1 + num2
-    def substract(self, num1, num2):
+    def subtract(self, num1, num2):
         return num1 - num2
     def multiply(self, num1, num2):
         return num1 * num2
